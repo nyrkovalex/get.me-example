e6a0747b5c2338ce7d633029d3aac1515ae7a760
Alexander Nyrkov <nyrkovalex@gmail.com>
2015-07-06 22:51:01 +0300

    version 0.0.2

--- src/main/java/com/github/nyrkovalex/get/me/example/GetMeExample.java
+++ src/main/java/com/github/nyrkovalex/get/me/example/GetMeExample.java
@@ -3,7 +3,7 @@ package com.github.nyrkovalex.get.me.example;
 public class GetMeExample {
 
 	public static void main(String[] args) {
-		System.out.println("Version 0.0.1");
+		System.out.println("Version 0.0.2");
 	}
 
 }
